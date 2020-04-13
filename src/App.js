bd962a0f81f6c21ca6ab882d65241034c3a6eae5
unknown <nichakan.tanang999@gmail.com>
2020-04-13 22:46:09 +0700

    Login

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component,useState,useEffect } from 'react';
 import './App.css';
 import firebase from "firebase"
 import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth"
@@ -8,45 +8,48 @@ firebase.initializeApp({
   authDomain:"miniproject079.firebaseapp.com"
 })
 
-class App extends Component{
-  state={isSignedIn: false}
-  uiConfig = {
-    signInFlow: "popup",
-    signInOptions:[
-      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-    ],
-    callbacks:{
-      signInSuccess: () => false
-    }
+const  uiConfig = {
+  signInFlow: "popup",
+  signInOptions:[
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+  ],
+  callbacks:{
+    signInSuccess: () => false
   }
+}
 
-  componentDidMount=()=>{
-
+const App =()=>{
+  const [isSignedIn,setisSignedIn] =useState(false);
+ 
+  useEffect(()=>{
     firebase.auth().onAuthStateChanged(user => {
-      this.setState({isSignedIn:!!user})
+      setisSignedIn(!!user)
       console.log(!!user)
     })
+  })
 
-  }
+ 
 
-  render (){
+  
     return(
     <div className="App">
-      {this.state.isSignedIn ? 
+      {isSignedIn ? 
       <span>
       <div>Signed In!</div>
       <button onClick={()=>firebase.auth().signOut()}>Log out</button>
       </span>
       :
-      (<StyledFirebaseAuth
-        uiConfig={this.uiConfig}
+      (
+      <StyledFirebaseAuth
+        uiConfig={uiConfig}
         firebaseAuth={firebase.auth()}
+        
       />)
       }
     </div>
   )
-    }
+    
 }
 
 export default App;
