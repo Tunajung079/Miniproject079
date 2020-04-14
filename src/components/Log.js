32cfcccffbb99b5c025a6169dbd4958a7614b0dc
unknown <nichakan.tanang999@gmail.com>
2020-04-15 06:28:38 +0700

    bootstrap

--- src/components/Log.js
+++ src/components/Log.js
@@ -1,12 +1,12 @@
 import React, { Component, useState, useEffect } from 'react'
-
+import { Button, Form, Nav, NavDropdown, Navbar, FormControl, Spinner, Icon } from 'react-bootstrap';
 const Log =()=>{
 
     return(
         <div> 
             
-            <center><img src="/static/media/logo1.f0bcf2bd.png" alt="5555" /></center>
-             <center>เข้าสู่เว็บไซต์</center>
+            <center><img src="/static/media/logo2.png" /></center>
+             <center><Button >Sign In</Button></center>
             
            
             
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,9 +1,33 @@
 import React, { Component, useState, useEffect } from 'react'
 import './Menu.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Form, Nav, NavDropdown, Navbar, FormControl, Spinner, Icon } from 'react-bootstrap';
 
 const Menu =()=>{
     return(
-        <div> Menu </div>
+        <div>
+         <Navbar bg="light" variant="light">
+            <Navbar.Brand href="#home">
+             <img
+                            src="/static/media/logo2.png" 
+                            width="100"
+                            height="80"
+                            className="d-inline-block align-top"
+                           
+                        />
+    </Navbar.Brand>
+    <Nav className="mr-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#features">Cuisine</Nav.Link>
+      <Nav.Link href="#pricing">Share your Food</Nav.Link>
+      <Nav.Link href="#pricing">About Us</Nav.Link>
+    </Nav>
+    <Nav>
+      <Nav.Link href="#deets">Log out</Nav.Link>
+    </Nav>
+    
+  </Navbar>
+        </div>
     )
 }
 
