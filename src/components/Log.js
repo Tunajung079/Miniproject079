f04c73d874fb69f7ce5b8bbe07b48e4d61238e19
unknown <nichakan.tanang999@gmail.com>
2020-04-14 17:05:45 +0700

    Test Logo

--- src/components/Log.js
+++ src/components/Log.js
@@ -1,9 +1,16 @@
 import React, { Component, useState, useEffect } from 'react'
 
-
 const Log =()=>{
+
     return(
-        <div> **Login** </div>
+        <div> 
+            
+            <center><img src="/static/media/logo1.f0bcf2bd.png" alt="5555" /></center>
+             <center>เข้าสู่เว็บไซต์</center>
+            
+           
+            
+            </div>
     )
 }
 
