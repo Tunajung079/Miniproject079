18971539c40071f9725f548fd7e030a7cdf7bfdd
unknown <nichakan.tanang999@gmail.com>
2020-04-16 08:42:42 +0700

    edit About

--- src/components/About.js
+++ src/components/About.js
@@ -3,19 +3,9 @@ import React, { Component, useState, useEffect } from 'react'
 const About = () => {
   return (
    
-    
-            <h5>FOLLOW US</h5>
-                            
-                                <li className="list-unstyled">
-                                    <a href="https://www.facebook.com/Na.Nichakan">Facebook</a>
-                                </li>
-                                <li className="list-unstyled">
-                                    <a href="mailto:nichakan.tanang999@.com">Email</a>
-                                </li>
-                                <li className="list-unstyled">
-                                    <a href="https://github.com/Tunajung079">Github</a>
-                                </li>
-  
+    <div>
+            Nichakan
+    </div>
   )
 }
 
